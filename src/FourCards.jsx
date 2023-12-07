971e7adcd36bffbd0edfd7029af732599aa08c61
Allan Musyoka <musyokaallan96@gmail.com>
2023-12-07 22:48:34 +0300

    Added desktop version

--- src/FourCards.jsx
+++ src/FourCards.jsx
@@ -8,27 +8,27 @@ function FourCards() {
 
     return(
 
-        <main className="border-2 border-blue-600 flex flex-col justify-center items-center py-[60px]">
+        <main className="flex flex-col justify-center items-center py-[60px] md:h-screen lg:h-auto">
 
             {/* Content Container */}
 
-            <div className="max-w-[90%] sm:max-w-[85%]">
+            <div className="max-w-[80%] w-[1400px]">
 
                 {/* Main Header */}
 
-                <header className="mainHeader text-center flex flex-col gap-5 pb-[80px]">
+                <header className="mainHeader text-center flex flex-col gap-5 pb-[80px] w-[750px] max-w-[100%] my-0 mx-auto">
 
-                    <h1 className="text-[2rem] leading-11">Reliable, efficient delivery <br/> <span>Powered by Technology</span></h1>
+                    <h1 className="text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] leading-11">Reliable, efficient delivery <br/> <span>Powered by Technology</span></h1>
 
-                    <p className="text-[1.3rem]">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+                    <p className="text-[1rem] sm:text-[1.3rem]">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
 
                 </header>
 
                 {/* Content Container */}
 
-                <div className="contentContainer grid grid-cols-1 grid-rows-4 grid-flow-col gap-9">
+                <div className="contentContainer grid grid-cols-1 grid-rows-4 grid-flow-col gap-9 md:grid-cols-2 md:grid-rows-2 md:justify-items-stretch lg:grid-rows-3 lg:grid-cols-3">
 
-                    <div className="px-9 py-7 rounded-lg">
+                    <div className="px-9 py-7 rounded-lg lg:row-start-2 lg:col-end-2">
 
                         <header className="contentHeader flex flex-col gap-2">
 
@@ -45,7 +45,7 @@ function FourCards() {
 
                     </div>
 
-                    <div className="px-9 py-7 rounded-lg">
+                    <div className="px-9 py-7 rounded-lg lg:col-start-2">
 
                         <header className="contentHeader flex flex-col gap-2">
 
@@ -62,7 +62,7 @@ function FourCards() {
 
                     </div>
 
-                    <div className="px-9 py-7 rounded-lg">
+                    <div className="px-9 py-7 rounded-lg lg:row-start-3 lg:col-start-2">
 
                         <header className="contentHeader flex flex-col gap-2">
 
@@ -79,7 +79,7 @@ function FourCards() {
 
                     </div>
 
-                    <div className="px-9 py-7 rounded-lg">
+                    <div className="px-9 py-7 rounded-lg lg:col-end-4 lg:row-start-2">
 
                         <header className="contentHeader flex flex-col gap-2">
 
